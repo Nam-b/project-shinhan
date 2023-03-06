25f3368420b3d0fbdf9cf87605d075a55ebfe521
mypiay123 <mypiay123@gmail.com>
2023-03-06 18:39:47 +0900

    3월 6일 헤더푸터 작업분

--- www/js/common.js
+++ www/js/common.js
@@ -1,13 +1,25 @@
+const body = document.querySelector('body');
 const header = document.querySelector('header');
 const gnb = document.querySelector('.gnb');
+const toggle_btn = document.querySelector('.gnb_toggle_btn');
 
-console.log(header);
-console.log(gnb);
+
+
+
+
+
 
 header.addEventListener('mouseover', function(){
-    this.classList.add('active');
+    if(!body.classList.contains('toggle_on')){
+        this.classList.add('active');
+    }
 });
 
 header.addEventListener('mouseout', function(){
     this.classList.remove('active');
+});
+
+toggle_btn.addEventListener('click', function(){
+    this.classList.toggle('on');
+    body.classList.toggle('toggle_on');
 });
