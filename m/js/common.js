168e5e0a292a919272b8ac93b5afe8e8b32c6866
mypiay123 <mypiay123@gmail.com>
2023-03-07 18:47:55 +0900

    3월 7일 작업분

---
+++ m/js/common.js
@@ -0,0 +1,18 @@
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const gnb = document.querySelector('.gnb');
+const toggle_btn = document.querySelector('.gnb_toggle_btn');
+const depth_01 = document.querySelectorAll('.depth_01 > li');
+
+
+
+toggle_btn.addEventListener('click', function(){
+    this.classList.toggle('on');
+    body.classList.toggle('toggle_on');
+});
+
+depth_01.forEach(function(e){
+    e.addEventListener('click', function(){
+        this.classList.toggle('on');
+    });
+});
